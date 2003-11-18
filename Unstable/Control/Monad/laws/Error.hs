929a9f38e98fca79a80fa3e87cc09df51c0c004e
diatchki <emertens@galois.com>
2003-11-18 09:31:56 -0800

    [project @ 2003-11-18 17:31:55 by diatchki]
    Did some space leak analisys.
    
    darcs-hash:20031118173156-f5001-7a629a2bb294ff9bf48a1a891b260e2aa15c9e45.gz

--- Unstable/Control/Monad/laws/Error.hs
+++ Unstable/Control/Monad/laws/Error.hs
@@ -9,5 +9,14 @@ handle_raise x h      = handle (raise x) h === h x
 handle_handle m h1 h2 = handle (handle m h1) h2 === handle m (\x -> handle (h1 x) h2)
 
 
+{-
+stateError x y        = when errorBeforeState $
+                          do set x
+                             z <- handle (set y >> raise error) (\_ -> get)
+                             assert (x === z)
+   
+-}
+
+
 
 
