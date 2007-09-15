21eafdeea2a47f41a5fb25d8a9bf6307d206b5ba
diatchki <diatchki@galois.com>
2007-09-15 12:41:39 -0700

    Reimplements Monads.hs in terms of deriving functions.
    darcs-hash:20070915194139-9637d-de2eb163ad49fb4876f0db7490883866af00d917.gz

--- src/Monads.hs
+++ src/Monads.hs
@@ -22,6 +22,12 @@ newtype State     i a     = S' { unS :: StateT      i Id a }
 newtype Exception i a     = X' { unX :: ExceptionT  i Id a }
 newtype Cont      i a     = C' { unC :: ContT       i Id a }
 
+iso_R = Iso R' unR
+iso_W = Iso W' unW
+iso_S = Iso S' unS
+iso_X = Iso X' unX
+iso_C = Iso C' unC
+
 instance               BaseM (Reader    i) (Reader    i) where inBase = id
 instance (Monoid i) => BaseM (Writer    i) (Writer    i) where inBase = id
 instance               BaseM (State     i) (State     i) where inBase = id
@@ -29,46 +35,46 @@ instance               BaseM (Exception i) (Exception i) where inBase = id
 instance               BaseM (Cont      i) (Cont      i) where inBase = id
 
 instance Monad (Reader i) where
-  return x    = R' (return x)
-  fail x      = R' (fail x)
-  m >>= f     = R' (unR m >>= (unR . f))
+  return  = derive_return iso_R
+  fail    = derive_fail iso_R
+  (>>=)   = derive_bind iso_R
 
 instance (Monoid i) => Monad (Writer i) where
-  return x    = W' (return x)
-  fail x      = W' (fail x)
-  m >>= f     = W' (unW m >>= (unW . f))
+  return  = derive_return iso_W
+  fail    = derive_fail iso_W
+  (>>=)   = derive_bind iso_W
 
 instance Monad (State i) where
-  return x    = S' (return x)
-  fail x      = S' (fail x)
-  m >>= f     = S' (unS m >>= (unS . f))
+  return  = derive_return iso_S
+  fail    = derive_fail iso_S
+  (>>=)   = derive_bind iso_S
 
 instance Monad (Exception i) where
-  return x    = X' (return x)
-  fail x      = X' (fail x)
-  m >>= f     = X' (unX m >>= (unX . f))
+  return  = derive_return iso_X
+  fail    = derive_fail iso_X
+  (>>=)   = derive_bind iso_X
 
 instance Monad (Cont i) where
-  return x    = C' (return x)
-  fail x      = C' (fail x)
-  m >>= f     = C' (unC m >>= (unC . f))
+  return  = derive_return iso_C
+  fail    = derive_fail iso_C
+  (>>=)   = derive_bind iso_C
 
-instance               Functor (Reader    i) where fmap = liftM
-instance (Monoid i) => Functor (Writer    i) where fmap = liftM
-instance               Functor (State     i) where fmap = liftM
-instance               Functor (Exception i) where fmap = liftM
-instance               Functor (Cont      i) where fmap = liftM
+instance               Functor (Reader    i) where fmap = derive_fmap iso_R
+instance (Monoid i) => Functor (Writer    i) where fmap = derive_fmap iso_W
+instance               Functor (State     i) where fmap = derive_fmap iso_S
+instance               Functor (Exception i) where fmap = derive_fmap iso_X
+instance               Functor (Cont      i) where fmap = derive_fmap iso_C
 
-instance               MonadFix (Reader    i) where mfix f = R' (mfix (unR . f))
-instance (Monoid i) => MonadFix (Writer    i) where mfix f = W' (mfix (unW . f))
-instance               MonadFix (State     i) where mfix f = S' (mfix (unS . f))
-instance               MonadFix (Exception i) where mfix f = X' (mfix (unX . f))
+instance               MonadFix (Reader    i) where mfix = derive_mfix iso_R
+instance (Monoid i) => MonadFix (Writer    i) where mfix = derive_mfix iso_W
+instance               MonadFix (State     i) where mfix = derive_mfix iso_S
+instance               MonadFix (Exception i) where mfix = derive_mfix iso_X
 
-instance ReaderM (Reader i) i where ask = R' ask
-instance (Monoid i) => WriterM (Writer i) i where put = W' . put 
-instance StateM (State i) i where get = S' get; set = S' . set 
-instance ExceptionM (Exception i) i where raise = X' . raise 
-instance ContM (Cont i) where callCC f = C' (callCC (unC . f . (C' .)))
+instance ReaderM (Reader i) i where ask = derive_ask iso_R
+instance (Monoid i) => WriterM (Writer i) i where put = derive_put iso_W
+instance StateM (State i) i where get = derive_get iso_S; set = derive_set iso_S
+instance ExceptionM (Exception i) i where raise = derive_raise iso_X
+instance ContM (Cont i) where callCC = derive_callCC iso_C
 
 runReader     :: i -> Reader i a -> a
 runWriter     :: Writer i a -> (a,i)
@@ -76,16 +82,20 @@ runState      :: i -> State i a -> (a,i)
 runException  :: Exception i a -> Either i a
 runCont       :: (a -> i) -> Cont i a -> i
 
-runReader i  = runId . runReaderT i          . unR 
+runReader i  = runId . runReaderT i          . unR
 runWriter    = runId . runWriterT            . unW
 runState  i  = runId . runStateT i           . unS
 runException = runId . runExceptionT         . unX
-runCont   i  = runId . runContT (return . i) . unC 
+runCont   i  = runId . runContT (return . i) . unC
 
-instance RunReaderM (Reader i) i where local i = R' . local i . unR
+instance RunReaderM (Reader i) i where
+  local = derive_local iso_R
+
 instance (Monoid i) => RunWriterM (Writer i) i where
-  collect = W' . collect . unW
-instance RunExceptionM (Exception i) i where try = X' . try     . unX
+  collect = derive_collect iso_W
+
+instance RunExceptionM (Exception i) i where
+  try = derive_try iso_X
 
 
 
