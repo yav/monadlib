b4434dd25313c546d651beae4dbf25349f835fcf
diatchki <diatchki@galois.com>
2007-09-15 12:40:56 -0700

    Add functions to derive functionality.
    darcs-hash:20070915194056-9637d-cc787c357e56bed774c29314e3b507f0be6b2ce9.gz

--- src/MonadLib.hs
+++ src/MonadLib.hs
@@ -27,6 +27,11 @@ module MonadLib (
   -- $Nested_Exec
   RunReaderM(..), RunWriterM(..), RunExceptionM(..),
 
+  -- * Automatically deriving functionality
+  Iso(..), derive_fmap, derive_return, derive_bind, derive_fail, derive_mfix,
+  derive_ask, derive_put, derive_get, derive_set, derive_raise, derive_callCC,
+  derive_local, derive_collect, derive_try,
+
   -- * Miscellaneous
   version,
   module Control.Monad
@@ -490,5 +495,55 @@ jump x (Lab k)      = k (x, Lab k) >> return unreachable
   where unreachable = error "(bug) jump: unreachable"
 
 
+-- | A isomorphism between (usually) monads.
+-- Typically the constructor and selector of a newtype delcaration.
+data Iso m n = Iso { close :: forall a. m a -> n a,
+                     open  :: forall a. n a -> m a }
+
+-- | Derive the implementation of 'fmap' from 'Functor'.
+derive_fmap :: (Functor m) => Iso m n -> (a -> b) -> n a -> n b
+derive_fmap iso f m = close iso (fmap f (open iso m))
+
+-- | Derive the implementation of 'return' from 'Monad'.
+derive_return :: (Monad m) => Iso m n -> (a -> n a)
+derive_return iso a = close iso (return a)
+
+-- | Derive the implementation of '>>=' from 'Monad'.
+derive_bind :: (Monad m) => Iso m n -> n a -> (a -> n b) -> n b
+derive_bind iso m k = close iso ((open iso m) >>= \x -> open iso (k x))
+
+derive_fail :: (Monad m) => Iso m n -> String -> n a
+derive_fail iso a = close iso (fail a)
+
+-- | Derive the implementation of 'mfix' from 'MonadFix'.
+derive_mfix :: (MonadFix m) => Iso m n -> (a -> n a) -> n a
+derive_mfix iso f = close iso (mfix (open iso . f))
+
+derive_ask :: (ReaderM m i) => Iso m n -> n i
+derive_ask iso = close iso ask
+
+derive_put :: (WriterM m i) => Iso m n -> i -> n ()
+derive_put iso x = close iso (put x)
+
+derive_get :: (StateM m i) => Iso m n -> n i
+derive_get iso = close iso get
+
+derive_set :: (StateM m i) => Iso m n -> i -> n ()
+derive_set iso x = close iso (set x)
+
+derive_raise :: (ExceptionM m i) => Iso m n -> i -> n a
+derive_raise iso x = close iso (raise x)
+
+derive_callCC :: (ContM m) => Iso m n -> ((a -> n b) -> n a) -> n a
+derive_callCC iso f = close iso (callCC (open iso . f . (close iso .)))
+
+derive_local :: (RunReaderM m i) => Iso m n -> i -> n a -> n a
+derive_local iso i = close iso . local i . open iso
+
+derive_collect :: (RunWriterM m i) => Iso m n -> n a -> n (a,i)
+derive_collect iso = close iso . collect . open iso
+
+derive_try :: (RunExceptionM m i) => Iso m n -> n a -> n (Either i a)
+derive_try iso = close iso . try . open iso
 
 
