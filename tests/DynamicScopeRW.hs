6a32799a1fc818cfb9634c69edaff57160571fd2
Iavor Diatchki <diatchki@vostro.(none)>
2009-12-04 10:35:17 -0800

    Update tests.
    - Illustrates the difference in the order of the transofrmers.
    - Illustrates what happens when we "jump".

--- tests/DynamicScopeRW.hs
+++ tests/DynamicScopeRW.hs
@@ -1,18 +1,34 @@
 import MonadLib
 
+testR () = twice $
+  do a <- ask
+     b <- local 'b' ask
+     c <- ask
+     d <- local 'c' ask
+     e <- ask
+     return (a,b,c,d,e)
 
-test1 = runId $ runReaderT 'a' $ runContT return $
-     do a <- ask
-        b <- local 'b' ask
-        c <- ask
-        d <- local 'c' ask
-        e <- ask
-        return (a,b,c,d,e)
+testRDyn  = runId $ runReaderT 'a'  $ runContT return $ testR ()
+testRStat = runId $ runContT return $ runReaderT 'a'  $ testR ()
 
-test2 = runId $ runWriterT $ runContT return $
+
+--------------------------------------------------------------------------------
+testW () = twice $
   do put "a"
      (_,w1) <- collect (put "b")
      put "c"
      (_,w2) <- collect (put "d")
      return (w1,w2)
+
+testWDyn  = runId $ runWriterT      $ runContT return $ testW ()
+testWStat = runId $ runContT return $ runWriterT      $ testW ()
+
+--------------------------------------------------------------------------------
+twice m = do
+  (loop, label) <- labelCC Nothing
+  x <- m
+  case loop of
+    Nothing -> jump (Just x) label
+    Just y -> return (y,x)
+
 
